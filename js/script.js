58371372661199d13e8f3e8de53669aaedd11099
Maxzb <47795883+Maxzb@users.noreply.github.com>
2019-11-09 20:29:11 +0300

    09.11.2019

--- js/script.js
+++ js/script.js
@@ -75,3 +75,11 @@ $('.films_cards').slick({
    autoplay: true,
    autoplaySpeed: 2500,
 });
+
+function slowScroll(id) {
+   var offset = 0;
+   $('html, body').animate({
+       scrollTop: $(id).offset().top - offset
+   }, 1000);
+   return false;
+}
